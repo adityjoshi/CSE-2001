6f7d8be3be4f0c8d749e6bfb92c55fe82870da30
Aditya Joshi <aditya03joshi@gmail.com>
2024-02-09 10:42:43 +0530

    10c-plus

---
+++ 10-cplus.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+inline bool isEligibleForVoting(int age) {
+    return age >= 18; 
+}
+
+int main() {
+    int candidateAge;
+    std::cout << "Enter candidate's age: ";
+    std::cin >> candidateAge;
+
+    if (isEligibleForVoting(candidateAge)) {
+        std::cout << "Candidate is eligible for voting." << std::endl;
+    } else {
+        std::cout << "Candidate is not eligible for voting." << std::endl;
+    }
+
+    return 0;
+}
+
